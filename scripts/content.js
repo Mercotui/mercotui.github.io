bb31e7f047ab8f6190e913d148762a9ce639c865
Menno van der Graaf <mennovandergraaf@hotmail.com>
2017-07-17 15:59:41 +0200

    start reimplementing page

--- scripts/content.js
+++
@@ -1,9 +0,0 @@
-function nav_articles_filler(){
-    var nav_articles = document.getElementById("nav_articles");
-    nav_articles.innerHTML = '<a class="mdl-navigation__link" href="content.html"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">subject</i>Article 0</a> \
-        <a class="mdl-navigation__link" href="content.html"><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">subject</i>Article 1</a>'
-}
-
-function dabadabaitdooo(){
-    
-}
