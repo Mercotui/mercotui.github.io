b19c84d5375c246ab140693a453ad47faa630ab2
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-08-04 15:01:18 +0200

    Replace tingle.js with baguetteBox.js for image gallery

--- scripts/potd.js
+++ scripts/potd.js
@@ -26,64 +26,16 @@ const imagelist = [
     }
 ]
 window.onload = function () {
-  let images = document.getElementsByClassName('potd');
+  baguetteBox.run('.gallery');
+//   let images = document.getElementsByClassName('potd');
+//
+//   for (let i = 0; i < images.length; i++) {
+//     images[i].addEventListener("click", onImageClick);
+//   }
+// }
+//
+// function onImageClick(event) {
+//   let element = event.target;
+//
 
-  for (let i = 0; i < images.length; i++) {
-    images[i].addEventListener("click", onImageClick);
-  }
-}
-
-function onImageClick(event) {
-  let element = event.target;
-  //
-  // var modal = new tingle.modal({
-  //     footer: true,
-  //     stickyFooter: false,
-  //     closeMethods: ['overlay', 'button', 'escape'],
-  //     closeLabel: "Close",
-  //     cssClass: ['custom-class-1', 'custom-class-2'],
-  //     onOpen: function() {
-  //         console.log('modal open');
-  //     },
-  //     onClose: function() {
-  //         console.log('modal closed');
-  //     },
-  //     beforeClose: function() {
-  //         // here's goes some logic
-  //         // e.g. save content before closing the modal
-  //         return true; // close the modal
-  //         return false; // nothing happens
-  //     }
-  // });
-  //
-  // // set content
-  // modal.setContent('<h1>here\'s some content</h1>');
-  //
-  // // add a button
-  // modal.addFooterBtn('Button label', 'tingle-btn tingle-btn--primary', function() {
-  //     // here goes some logic
-  //     modal.close();
-  // });
-  //
-  // // add another button
-  // modal.addFooterBtn('Dangerous action !', 'tingle-btn tingle-btn--danger', function() {
-  //     // here goes some logic
-  //     modal.close();
-  // });
-  //
-  // // open modal
-  // modal.open();
-
-  var modal = new tingle.modal({
-    footer: false,
-    stickyFooter: false,
-    closeMethods: ['overlay', 'button', 'escape'],
-    closeLabel: "Close",
-    // cssClass: ['modal-image']
-    });
-
-    // set content
-    modal.setContent('<img src="'+element.src+'" class="modal-image"></img>');
-    modal.checkOverflow();
-    modal.open();
 }
