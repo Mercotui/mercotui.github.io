ed765d5392bb12aa4ff7ea403a38367344c66dc8
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-08-06 00:51:41 +0200

    Add randomness to homepage potd

--- assets/potdscript.js
+++ assets/potdscript.js
@@ -5,5 +5,32 @@ window.onload = function () {
 }
 
 function select_random_potd () {
-  console.log("test!");
+  let element = document.getElementById('potd-random');
+
+  if (element){
+    var daily_random_number = javascript_is_dumb_random(day());
+    daily_random_number = Math.floor(daily_random_number *= imagelist.length);
+
+    let random_image = imagelist[daily_random_number];
+
+    element.src = random_image.url;
+    element.className = "potd";
+
+    let container_element = document.getElementById('potd-random-container');
+    if (container_element) {
+      container_element.href = random_image.url;
+
+    }
+  }
+}
+
+function day() {
+  var now = new Date();
+  var fullDaysSinceEpoch = Math.floor(now/8.64e7);
+  return fullDaysSinceEpoch;
+}
+
+function javascript_is_dumb_random(seed) {
+    var x = Math.sin(seed) * 10000;
+    return x - Math.floor(x);
 }
