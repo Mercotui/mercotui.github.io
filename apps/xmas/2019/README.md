556e711faa8e887e4313824a268b33265c9a235c
Menno van der Graaf <mennovandergraaf@hotmail.com>
2019-12-19 04:31:32 +0100

    Implement app 2019

--- apps/xmas/2019/README.md
+++ apps/xmas/2019/README.md
@@ -6,3 +6,7 @@ Because I secretly enjoy it a lot.
 Silent Night Kevin MacLeod (incompetech.com)
 Licensed under Creative Commons: By Attribution 3.0 License
 http://creativecommons.org/licenses/by/3.0/
+
+## Images
+
+[Bridge svg](https://nl.m.wikipedia.org/wiki/Bestand:Arch_bridge_icon.svg)
--- apps/xmas/2019/script.js
+++ apps/xmas/2019/script.js
@@ -1,26 +1,133 @@
 window.addEventListener('resize', function(){
-  jsCanvasSnow.resize(window.innerWidth, window.innerHeight);
+    const size = document.getElementById('content').offsetWidth;
+    jsCanvasSnow.resize(size, size);
     jsCanvasSnow.init();
 }, false);
 
 window.addEventListener('load', function(){
-  jsCanvasSnow.init();
-
-  var canvas = document.getElementById('particle_canvas');
   var audio = new Audio('silentnight.mp3');
+
+  var query_string = window.location.search.substr(1);
+  var hash_string = window.location.hash.substr(1);
+  var decoded_string = window.atob(query_string);
+  set_greetings(decoded_string, hash_string);
+
+  jsCanvasSnow.init();
+  const size = document.getElementById('content').offsetWidth;
+  jsCanvasSnow.resize(size, size);
   jsCanvasSnow.start();
 
-  canvas.onmousedown = function(){
+  // Event handlers
+  document.ontouchstart = function(){
+    animate_lightup();
     jsCanvasSnow.startsnow();
     audio.play();
-  }
-
-  canvas.onmouseup = function(){
+  };
+  document.ontouchend = function(){
+    animate_shutdown();
     audio.pause();
     jsCanvasSnow.stopsnow();
-  }
+  };
+
+  document.onmousedown = document.ontouchstart;
+  document.onmouseup = document.ontouchend;
 }, false);
 
+function set_greetings(custom_text, name_index){
+  var text_1 = document.getElementById('text_1');
+  var text_2 = document.getElementById('text_2');
+  var text_3 = document.getElementById('text_3');
+  var text_4 = document.getElementById('text_4');
+
+  var lines =  custom_text.split('|');
+  if (lines.lenght == 1){
+    text_2.textContent = lines[0];
+  } else {
+    text_1.textContent = lines[0];
+    text_2.textContent = lines[1];
+  }
+
+  switch (name_index) {
+    case '1':
+      text_3.textContent = "Season's Greetings,";
+      text_4.textContent = "Mercotui";
+      break;
+    case '2':
+      text_3.textContent = "Bork Bork Bork,";
+      text_4.textContent = "Mercotui";
+      break;
+    case '3':
+      text_3.textContent = "Liefs,";
+      text_4.textContent = "Menno";
+      break;
+    default:
+  }
+}
+
+function animate_lightup() {
+  var back_light = document.getElementById('back_light');
+  back_light.classList.remove('fadeout');
+  back_light.classList.add('fadein');
+
+  var tree = document.getElementById('tree');
+  tree.classList.remove('fadeout');
+  tree.classList.add('fadein');
+
+  var text_1 = document.getElementById('text_1');
+  text_1.classList.remove('fadeout');
+  text_1.classList.add('fadein');
+
+  var text_2 = document.getElementById('text_2');
+  text_2.classList.remove('fadeout');
+  text_2.classList.add('fadein');
+
+  var text_hint = document.getElementById('text_hint');
+  text_hint.classList.remove('fadein');
+  text_hint.classList.add('fadeout');
+
+  var text_3 = document.getElementById('text_3');
+  text_3.classList.remove('fadeout');
+  text_3.classList.add('fadein');
+
+  var text_4 = document.getElementById('text_4');
+  text_4.classList.remove('fadeout');
+  text_4.classList.add('fadein');
+}
+
+function animate_shutdown() {
+  var back_light = document.getElementById('back_light');
+  back_light.classList.remove('fadein');
+  back_light.classList.add('fadeout');
+
+  var tree = document.getElementById('tree');
+  tree.classList.remove('fadein');
+  tree.classList.add('fadeout');
+
+  var text_1 = document.getElementById('text_1');
+  text_1.classList.remove('fadein');
+  text_1.classList.add('fadeout');
+
+  var text_2 = document.getElementById('text_2');
+  text_2.classList.remove('fadein');
+  text_2.classList.add('fadeout');
+
+  var text_hint = document.getElementById('text_hint');
+  // text_hint.classList.remove('fadeout');
+  // text_hint.classList.add('fadein');
+
+  var text_3 = document.getElementById('text_3');
+  text_3.classList.remove('fadein');
+  text_3.classList.add('fadeout');
+
+  var text_4 = document.getElementById('text_4');
+  text_4.classList.remove('fadein');
+  text_4.classList.add('fadeout');
+
+  // document.getElementById('text_hint').classList.add('fadeout');
+  // var marginLeft = window.getComputedStyle(boxOne).getPropertyValue('margin-left');
+  // boxOne.style.marginLeft = marginLeft;
+  // boxOne.classList.remove('horizTranslate');
+}
 
 function jsParticle(origin, velocity, size, amplitude)
 {
@@ -57,23 +164,21 @@ var jsCanvasSnow =
 	init : function( )
 	{
 		// use the container width and height
-		this.canvas = document.getElementById('particle_canvas');
+		this.canvas = document.getElementById('snow_canvas');
 		this.ctx = this.canvas.getContext('2d');
-    this.resize(window.innerWidth, window.innerHeight);
 
 		// change these values
     // the 2 element arrays represent min and max values
 		this.pAmount = 1000;         // amount of particles
-		this.pSize = [0.5 , 1.5];    // min and max size
+		this.pSize = [0.5 , 2.0];    // min and max size
 		this.pSwing = [0.1, 1];      // min and max oscilation speed for x movement
-		this.pSpeed = [40, 100],     // min and max y speed
+		this.pSpeed = [10, 50],     // min and max y speed
 		this.pAmplitude = [25, 50];  // min and max distance for x movement
-
-    this._init_particles();
 	},
 
 	start : function()
 	{
+    this._init_particles();
 		this.running = true;
 		this.start_time = this.frame_time = microtime();
 		this._loop();
@@ -130,7 +235,7 @@ var jsCanvasSnow =
 
   _fast_clear_falling_particles : function()
   {
-    // reinit any fallen particle
+    // stop any falling particles from entering the view
     for ( var i = 0 ; i < this.particles.length ; i++)
     {
       var particle = this.particles[i];
