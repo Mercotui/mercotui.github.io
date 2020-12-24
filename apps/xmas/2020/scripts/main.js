2bc74ef95efcca302c497cc7c802474d42378eb4
Mercotui <Mercotui@users.noreply.github.com>
2020-12-24 23:51:12 +0000

    Publishing Site Mercotui/mercotui.github.io at 78a8ecd1d49f36ab325bb48ab30b59fec3276e01 on Thu Dec 24 23:51:12 UTC 2020

---
+++ apps/xmas/2020/scripts/main.js
@@ -0,0 +1,243 @@
+window.addEventListener('resize', function(){
+    // const size = document.getElementById('content').offsetWidth;
+    snow_renderer.resize();
+    water_renderer.resize();
+}, false);
+
+window.addEventListener('load', function(){
+  var audio = new Audio('assets/theAmbientVisitor-Crimble.mp3');
+  audio.volume = 0.0;
+
+  var query_string = window.location.search.substr(1);
+  var hash_string = window.location.hash.substr(1);
+  var decoded_string = window.atob(query_string);
+  var playing = false;
+  var muted = false;
+  set_greetings(decoded_string, hash_string);
+
+  snow_renderer.init();
+  water_renderer.init();
+
+  // Event handlers
+  var button_play = document.getElementById('buttonPlay');
+  button_play.onclick = function(event){
+    if (controlls_enabled === true) {
+      event.stopImmediatePropagation();
+      if (playing != true){
+        playing = true;
+        animate_cycle();
+        animate_lightup();
+        audio_fade(audio, 1.0);
+
+        snow_renderer.start();
+        water_renderer.start();
+
+        button_play.src = "assets/pause.svg";
+      } else {
+        playing = false;
+        animate_shutdown();
+        audio_fade(audio, 0.0);
+
+        snow_renderer.stop();
+        water_renderer.stop();
+
+        button_play.src = "assets/play.svg";
+      }
+
+      if (playing && !muted){
+        audio_fade(audio, 1.0);
+      } else {
+        audio_fade(audio, 0.0);
+      }
+    }
+  }
+
+  var button_mute = document.getElementById('buttonMute');
+  button_mute.onclick = function(event){
+    if (controlls_enabled === true) {
+      event.stopImmediatePropagation();
+      if (muted) {
+        muted = false;
+        button_mute.src = "assets/mute.svg";
+      } else {
+        muted = true;
+        button_mute.src = "assets/music.svg";
+      }
+
+      if (playing && !muted){
+        audio_fade(audio, 1.0);
+      } else {
+        audio.volume = 0;
+        audio.pause();
+      }
+    }
+  }
+
+  var button_info = document.getElementById('buttonInfo');
+  button_info.onclick = function(event){
+    if (controlls_enabled === true) {
+      event.stopImmediatePropagation();
+      window.open('https://github.com/Mercotui/mercotui.github.io/tree/master/apps/xmas/2020', '_blank');
+    }
+  }
+
+
+  document.onclick = function() {
+    show_controls();
+    start_controls_timer();
+  }
+  show_controls();
+}, false);
+
+var fade_interval;
+function audio_fade(audio, setpoint){
+  clearInterval(fade_interval);
+  audio.play();
+
+  var setVolume = setpoint;
+  var delta = setVolume - audio.volume;
+  if (delta > 0.0){
+    var speed = 0.1;
+  } else {
+    var speed = -0.01;
+  }
+
+  fade_interval = setInterval(function(){
+    var new_volume = audio.volume + speed;
+    new_volume = Math.max(new_volume,0.0);
+    new_volume = Math.min(new_volume,1.0);
+    audio.volume = new_volume;
+
+    if (delta > 0.0) {
+      if (audio.volume >= setVolume || audio.volume >= 1.0) {
+        clearInterval(fade_interval);
+      }
+    } else {
+      if (audio.volume <= setVolume || audio.volume <= 0.0) {
+        clearInterval(fade_interval);
+        audio.pause();
+      }
+    }
+  },50);
+};
+
+function set_greetings(custom_text, name_index){
+  var text_1 = document.getElementById('text_1');
+  var text_2 = document.getElementById('text_2');
+  var text_3 = document.getElementById('text_3');
+
+  var lines =  custom_text.split('|');
+  if (lines.length == 1 && (lines[0].length > 0)){
+    text_2.textContent = lines[0];
+  } else if (lines.length == 2){
+    text_1.textContent = lines[0];
+    text_2.textContent = lines[1];
+  }
+
+
+  switch (name_index) {
+    case '1':
+      text_3.textContent = "Mercotui";
+      break;
+    case '2':
+      text_3.textContent = "Menno";
+      break;
+    case '3':
+      text_3.textContent = "Liefs, Menno";
+      break;
+    default:
+  }
+}
+
+var cycle_started = false;
+function animate_cycle() {
+  if (!cycle_started) {
+    cycle_started = true;
+
+    setInterval(function () {
+      var ubahn = document.getElementById('ubahn');
+      ubahn.classList.remove('cycle');
+      void ubahn.offsetWidth;
+      ubahn.classList.add('cycle');
+    }, 25000);
+  }
+}
+
+function animate_lightup() {
+  var back_light = document.getElementById('back_light');
+  back_light.classList.remove('fadeout');
+  back_light.classList.add('fadein');
+
+  var bridge = document.getElementById('bridge');
+  bridge.classList.remove('fadeout');
+  bridge.classList.add('fadein');
+
+  var water = document.getElementById('water_canvas');
+  water.classList.remove('fadeout');
+  water.classList.add('fadein');
+
+  var text_1 = document.getElementById('text_1');
+  text_1.classList.remove('fadeout');
+  text_1.classList.add('fadein');
+
+  var text_2 = document.getElementById('text_2');
+  text_2.classList.remove('fadeout');
+  text_2.classList.add('fadein');
+
+  var text_3 = document.getElementById('text_3');
+  text_3.classList.remove('fadeout');
+  text_3.classList.add('fadein');
+
+  var controlls = document.getElementById('controlls');
+  controlls.classList.remove('fadein');
+  controlls.classList.add('fadeout');
+}
+
+function animate_shutdown() {
+  var back_light = document.getElementById('back_light');
+  back_light.classList.remove('fadein');
+  back_light.classList.add('fadeout');
+
+  var bridge = document.getElementById('bridge');
+  bridge.classList.remove('fadein');
+  bridge.classList.add('fadeout');
+
+  var water = document.getElementById('water_canvas');
+  water.classList.remove('fadein');
+  water.classList.add('fadeout');
+
+  var text_1 = document.getElementById('text_1');
+  text_1.classList.remove('fadein');
+  text_1.classList.add('fadeout');
+
+  var text_2 = document.getElementById('text_2');
+  text_2.classList.remove('fadein');
+  text_2.classList.add('fadeout');
+
+  var text_3 = document.getElementById('text_3');
+  text_3.classList.remove('fadein');
+  text_3.classList.add('fadeout');
+}
+
+var controlls_timeout;
+var controlls_enabled;
+function start_controls_timer() {
+  clearInterval(controlls_timeout);
+  controlls_timeout = setTimeout(hide_controls, 4000);
+}
+
+function hide_controls() {
+  var controlls = document.getElementById('controlls');
+  controlls.classList.remove('fadein');
+  controlls.classList.add('fadeout');
+
+  controlls_enabled = false;
+}
+
+function show_controls() {
+  var controlls = document.getElementById('controlls');
+  controlls.classList.remove('fadeout');
+  controlls.classList.add('fadein');
+
+  controlls_enabled = true;
+}
---
+++ apps/xmas/2020/scripts/snow.js
@@ -0,0 +1,144 @@
+class Flake {
+  constructor(width, height, size, speed, delay, color){
+    this.start_delay = delay;
+    this.color = color
+  	this.speed = speed;
+  	this.size = size;
+    this.tick = -this.start_delay;
+    this.opacity = 0;
+    this.canvas_width = width;
+    this.canvas_height = height;
+
+    this.randomPosition = function() {
+      return new Vector2(frand(0, this.canvas_width - 1), frand(0, this.canvas_height - 1));
+    }
+    this.position = this.randomPosition();
+
+  	this.update = function(delta_time, running){
+      if (running) {
+        this.tick += delta_time * speed;
+        if (this.tick > 6.28) { // after each cycle reset and relocate
+          this.tick = this.tick % 6.28;
+          this.position = this.randomPosition();
+        }
+        if (this.tick >= 0) {
+		        this.opacity = (Math.cos(this.tick) - 1) * -0.5;
+        }
+      } else {
+        this.tick = 0;
+        this.opacity =  Math.max((this.opacity - delta_time*speed), 0);
+      }
+  	};
+  };
+};
+
+var snow_renderer =
+{
+	canvas : null,
+	ctx : null,
+  mask : null,
+  mask_ctx : null,
+	flakes : [],
+	running : false,
+  snowing : true,
+	start_time : 0,
+	frame_time : 0,
+
+	init : function( )
+	{
+		// use the container width and height
+		this.canvas = document.getElementById('snow_canvas');
+		this.ctx = this.canvas.getContext('2d');
+    this.resize();
+
+		this.flake_amount = 25;          // amount of flakes
+		this.flake_size   = [this.canvas.width / 150 , this.canvas.width / 10]; // min and max size
+		this.flake_speed  = [0.005, 0.5];      // min and max blink speed
+    this.flake_delay  = [2,10];
+		this.flake_colors = ["#4D0A21", "#700814", "#C11F1F", "#AB5457", "#CEDEDF",
+                        "#FFE77A", "#D6AE29", "#00661F", "#00661F", "#1E4812"];
+
+    this._init_flakes();
+	},
+
+	start : function()
+	{
+		this.running = true;
+		this.start_time = this.frame_time = microtime();
+		this._loop();
+	},
+
+	stop : function()
+	{
+		this.running = false;
+	},
+
+  resize : function()
+  {
+    const canvas = document.getElementById('snow_canvas');
+    this.canvas.width = canvas.offsetWidth;
+    this.canvas.height = canvas.offsetHeight;
+  },
+
+	_loop : function()
+	{
+		snow_renderer._update();
+		snow_renderer._draw();
+		snow_renderer._queue();
+
+	},
+
+	_init_flakes : function()
+	{
+		// clear the flakes array
+		this.flakes.length = 0;
+
+		for ( var i = 0 ; i < this.flake_amount ; i++)
+		{
+			var speed = frand(this.flake_speed[0], this.flake_speed[1]);
+			var size = frand(this.flake_size[0], this.flake_size[1]);
+      var delay = frand(this.flake_delay[0], this.flake_delay[1]);
+      var color = this.flake_colors[Math.floor(Math.random() * this.flake_colors.length)];
+
+			this.flakes.push(new Flake(this.canvas.width, this.canvas.height, size, speed, delay, color));
+		}
+	},
+
+	_update : function()
+	{
+		// calculate the time since the last frame
+		var now_time = microtime();
+		var delta_time = now_time - this.frame_time;
+
+		for ( var i = 0 ; i < this.flakes.length ; i++)
+		{
+			var flake = this.flakes[i];
+      flake.update(delta_time, this.running);
+		}
+
+		// save this time for the next frame
+		this.frame_time = now_time;
+	},
+
+	_draw : function()
+	{
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+		for ( var i = 0 ; i < this.flakes.length ; i++) {
+      var flake = this.flakes[i];
+
+      this.ctx.beginPath();
+  		this.ctx.arc(flake.position.x - (flake.size/2), flake.position.y - (flake.size/2), flake.size, 0, 2 * Math.PI);
+  		this.ctx.closePath();
+
+      var opacity = ("00" + Math.round(flake.opacity * 0xFF).toString(16)).slice(-2);
+      this.ctx.fillStyle = flake.color + opacity;
+      this.ctx.fill();
+      // this.ctx.fillRect(flake.position.x - (flake.size/2), flake.position.y - (flake.size/2), flake.size, flake.size);
+    }
+	},
+
+	_queue : function()
+	{
+		window.requestAnimationFrame( snow_renderer._loop );
+	}
+};
